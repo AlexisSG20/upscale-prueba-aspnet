c4c38e7e174415428703900723daec0a0980071a
AlexisSG20 <90637515+AlexisSG20@users.noreply.github.com>
2026-03-22 13:16:37 -0500

    feat(web): agregar vista de perfil de usuario

---
+++ Models/PerfilUsuarioViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace SistemaAccesoWeb.Models
+{
+    public class PerfilUsuarioViewModel
+    {
+        public string TipoDocumento { get; set; } = string.Empty;
+        public string NumeroDocumento { get; set; } = string.Empty;
+        public string Nombres { get; set; } = string.Empty;
+        public string ApellidoPaterno { get; set; } = string.Empty;
+        public string ApellidoMaterno { get; set; } = string.Empty;
+        public string CorreoPrincipal { get; set; } = string.Empty;
+        public string? CorreoSecundario { get; set; }
+        public string TelefonoMovil { get; set; } = string.Empty;
+        public string? TelefonoSecundario { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string Nacionalidad { get; set; } = string.Empty;
+        public string Sexo { get; set; } = string.Empty;
+        public string TipoContratacion { get; set; } = string.Empty;
+        public DateTime? FechaContratacion { get; set; }
+        public string Entidad { get; set; } = string.Empty;
+        public string Rol { get; set; } = string.Empty;
+        public string Estado { get; set; } = string.Empty;
+    }
+}
